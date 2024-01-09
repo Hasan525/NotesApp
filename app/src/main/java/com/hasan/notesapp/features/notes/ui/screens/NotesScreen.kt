f8ffea1c483676759c46e1e6c890ab7ab4b6a3a4
Hasan <hasan@inverseai.com>
2024-01-09 22:54:49 +0600

    -add a dialog design

--- app/src/main/java/com/hasan/notesapp/features/notes/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/hasan/notesapp/features/notes/ui/screens/NotesScreen.kt
@@ -1,17 +1,23 @@
 package com.hasan.notesapp.features.notes.ui.screens
 
+import android.icu.text.CaseMap.Title
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -20,13 +26,17 @@ import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -34,6 +44,7 @@ import androidx.compose.ui.unit.sp
 import com.hasan.notesapp.data.model.NotesResponse
 import com.hasan.notesapp.ui.theme.Background
 import com.hasan.notesapp.ui.theme.ContentColor
+import com.hasan.notesapp.ui.theme.Red
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -63,6 +74,55 @@ fun NotesScreen() {
     }
 }
 
+
+@Composable
+fun showDialogBox(
+    title: String,
+    description: String,
+    onTitleChange: (String) -> Unit,
+    onDescriptionChange: (String) -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = { /*TODO*/ },
+        confirmButton = { /*TODO*/ },
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier
+            .background(Background),
+        text = {
+            IconButton(
+                onClick = {
+                    /*TODO*/
+                }
+            ) {
+                Icon(imageVector = Icons.Default.Close, contentDescription = "", tint = Red)
+            }
+        }
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTextField(
+    text: String,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit
+) {
+    TextField(
+        value = text,
+        onValueChange = onValueChange,
+        modifier = modifier.fillMaxWidth(),
+        colors = TextFieldDefaults.textFieldColors(
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent
+        ),
+        placeholder = {
+            Text(text = placeholder, color = Color.Black.copy(alpha = 0.4f))
+        }
+    )
+}
+
 @Composable
 fun NotesEachRow(
     notesResponse: NotesResponse,
@@ -76,17 +136,47 @@ fun NotesEachRow(
                 color = ContentColor,
                 shape = RoundedCornerShape(8.dp)
             )
-    ){
-        Column {
+    ) {
+        Column(
+            modifier = Modifier.padding(15.dp)
+        )
+        {
             Row(
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Text(text = notesResponse.title, style = TextStyle(
-                    color = Color.Black,
-                    fontSize = 22.sp,
-                    fontWeight = FontWeight.W600
-                ))
+                Text(
+                    text = notesResponse.title, style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.W600
+                    ), modifier = Modifier.weight(0.7f)
+                )
+                IconButton(
+                    onClick = {
+                        /*TODO*/
+                    },
+                    modifier = Modifier
+                        .weight(0.3f)
+                        .align(CenterVertically)
+                ) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = "", tint = Red)
+                }
             }
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = notesResponse.description, style = TextStyle(
+                    color = Color.Black.copy(alpha = 0.6f),
+                    fontSize = 14.sp
+                )
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = notesResponse.updated_at.split("T")[0], style = TextStyle(
+                    color = Color.Black.copy(alpha = 0.3f),
+                    fontSize = 10.sp
+                )
+            )
+
         }
     }
 }
